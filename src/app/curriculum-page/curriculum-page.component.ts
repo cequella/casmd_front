eec4dd4fa1afad6747961d8da6cd9f2c5a2f2a0f
cequella <pedro.oliveira.demolay@gmail.com>
2018-09-11 15:38:15 -0300

    Update on curriculum page

--- src/app/curriculum-page/curriculum-page.component.ts
+++ src/app/curriculum-page/curriculum-page.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Course }            from '../models/Course';
+import { Curriculum }        from '../models/Curriculum';
+import { CurriculumService } from '../webservice/curriculum.service';
+
 @Component({
-  selector: 'app-curriculum-page',
-  templateUrl: './curriculum-page.component.html',
-  styleUrls: ['./curriculum-page.component.scss']
+    selector: 'app-curriculum-page',
+    templateUrl: './curriculum-page.component.html',
+    styleUrls: ['./curriculum-page.component.scss']
 })
 export class CurriculumPageComponent implements OnInit {
+    curriculum: Curriculum;
 
-  constructor() { }
+    constructor(private service: CurriculumService) { }
 
-  ngOnInit() {
-  }
+    ngOnInit() {
+	this.service
+	    .getEpoch("2016.1")
+	    .subscribe(curriculum => this.curriculum =curriculum);
+    }
 
 }
