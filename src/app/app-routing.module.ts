aef9a2dac019c9bd1634e6cc20c78c91fbdff2fe
cequella <pedro.oliveira.demolay@gmail.com>
2018-10-18 22:54:54 -0300

    update

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent } from './home-page/home-page.component';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 const routes: Routes = [
-    { path: '', component: HomePageComponent }
+    { path: '',      component: HomePageComponent  },
+    { path: 'sobre', component: AboutPageComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,26 +4,33 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { HomePageComponent }  from './home-page/home-page.component';
+import { LoginPageComponent } from './login-page/login-page.component';
+
 import { AppComponent } from './app.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ConstructionPageComponent } from './construction-page/construction-page.component';
 import { MaterialModulesModule } from './material-modules/material-modules.module';
-import { HomePageComponent } from './home-page/home-page.component';
 import { AppRoutingModule } from './/app-routing.module';
 import { HomeHeaderComponent } from './home-header/home-header.component';
 import { TimeLineComponent } from './time-line/time-line.component';
 import { NewHeadlineComponent } from './new-headline/new-headline.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { AboutPageComponent } from './about-page/about-page.component';
 
 @NgModule({
     declarations: [
 	AppComponent,
 	ConstructionPageComponent,
+
 	HomePageComponent,
+	LoginPageComponent,
+
 	HomeHeaderComponent,
 	TimeLineComponent,
-	NewHeadlineComponent
+	NewHeadlineComponent,
+	AboutPageComponent
     ],
     imports: [
 	BrowserModule,
