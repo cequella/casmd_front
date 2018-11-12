d9d73719e94e3f8b84ac73cfcf08d45b9b8157ff
cequella <pedro.oliveira.demolay@gmail.com>
2018-11-12 12:26:09 -0300

    Corrections

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,12 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { HomePageComponent }          from './home-page/home-page.component';
 import { DocumentationPageComponent } from './documentation-page/documentation-page.component';
-import { LoginPageComponent }         from './login-page/login-page.component';
 
 const routes: Routes = [
     { path: '', component: HomePageComponent },
-    { path: 'documentos', component: DocumentationPageComponent },
-    { path: 'admin/login', component: LoginPageComponent }
+    { path: 'documentos', component: DocumentationPageComponent }
 ];
 
 @NgModule({
